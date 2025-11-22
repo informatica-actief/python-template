9b0a5497a58e081d4a54b3677d9c791f6450b98d
Sander van Geest <49232516+vangeest@users.noreply.github.com>
2025-11-22 23:07:07 +0100

    voorbeeld programma met turtle

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-#
-# Kort voorbeeldprogramma in pyhton met turtle package
-#
+# Python program to draw square 
+# using Turtle Programming
+import turtle 
+skk = turtle.Turtle()
+
+for i in range(4):
+    skk.forward(50)
+    skk.right(90)
+    
+turtle.done()
